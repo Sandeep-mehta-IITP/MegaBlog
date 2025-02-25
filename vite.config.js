f6c8aaedf33f0eea450ac3ae48f1cea0cd7c16db
Sandeep-mehta-IITP <sandeep797671@gmail.com>
2025-02-25 11:35:06 +0530

    update vite config and add new file netlify

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure correct file paths
+  build: {
+    assetsDir: 'assets', // Fixes incorrect MIME types
+  },
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000
-  }
-})
+    port: process.env.PORT || 3000,
+  },
+});
+
 
 
